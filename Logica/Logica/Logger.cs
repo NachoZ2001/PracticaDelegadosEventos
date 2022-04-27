094f19a8335314c8899eb323221cef3b7899d0ff
ignacio zbrun <ignazbrun5@gmail.com>
2022-04-27 19:57:36 -0300

    Ejercicio 1 listo

---
+++ Logica/Logica/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class Logger
+    {
+        public static void ImprimirEnConsola(object? sender, NombresEventArgs nombres)
+        {
+            Console.WriteLine($"El nombre fue modificado de {nombres.NombreAntiguo} a {nombres.NombreModificado}");
+        }
+    }
+}
---
+++ Logica/Logica/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Persona persona = new Persona("Pedro");
+            persona.ActualizarNombre("Ignacio");
+            persona.NombreModificado += Logger.ImprimirEnConsola;
+            persona.NombreModificado += CalculoLetras.CalcularLetras;
+            Console.Read();
+        }
+ 
+    }
+}
