d08b703e513ebb1c2cf9c7ce06974423784f82cd
nachozbrun <ignazbrun5@gmail.com>
2022-04-28 22:57:12 -0300

    Ejercicio 1, 2 y 3 listo

--- Logica/Logica/Program.cs
+++ Logica/Logica/Program.cs
@@ -3,19 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Logica;
 
 namespace Logica
 {
-    class Program
-    {
-        static void Main(string [] args)
+        public class CalculoLetras
         {
-            Persona persona = new Persona("Pedro");
-            persona.ActualizarNombre("Ignacio");
-            persona.NombreModificado += Logger.ImprimirEnConsola;
-            persona.NombreModificado += CalculoLetras.CalcularLetras;
-            Console.Read();
+            public static void CalcularLetras(object? sender, NombresEventArgs nombres)
+            {
+                int cont = 0;
+                foreach (char letraNombreModificado in nombres.NombreModificado)
+                {
+                    int ban = 0;
+                    foreach (char letraNombreAntiguo in nombres.NombreAntiguo)
+                    {
+                        if (letraNombreModificado == letraNombreAntiguo)
+                        {
+                            ban = 1;
+                        }
+                    }
+                    if (ban == 0)
+                    {
+                        cont++;
+                    }
+                }
+                Console.WriteLine($"La cantidad de letras que tiene el nuevo nombre que el anterior no tiene son {cont}");
+            }
         }
- 
-    }
+
+        public class Logger
+        {
+            public static void ImprimirEnConsola(object? sender, NombresEventArgs nombres)
+            {
+                Console.WriteLine($"El nombre fue modificado de {nombres.NombreAntiguo} a {nombres.NombreModificado}");
+            }
+        }
+
+        public class Persona
+        {
+            public EventHandler<NombresEventArgs> NombreModificado;
+            private string Nombre { get; set; }
+
+            public Persona() { }
+            public Persona(string Nombre)
+            {
+                this.Nombre = Nombre;
+            }
+            public bool ActualizarNombre(string nuevoNombre)
+            {
+                string nombreAnterior = Nombre;
+                Nombre = nuevoNombre;
+                if (this.NombreModificado != null)
+                {
+                    this.NombreModificado(this, new NombresEventArgs { NombreAntiguo = nombreAnterior, NombreModificado = nuevoNombre });
+                }
+                return true;
+            }
+        }
+        public class NombresEventArgs : EventArgs
+        {
+            public string NombreAntiguo { get; set; }
+            public string NombreModificado { get; set; }
+        }
+    
 }
